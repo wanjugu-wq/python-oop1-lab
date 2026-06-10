0986173cc36d7fa7b233afd4ae76cf3e6a146df4
michelle.wanjugu <wanjuguzzz+work@gmail.com>
2026-06-10 12:31:08 +0300

    done w coffee.py

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self._size = size
+        self.price = price
+
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self,value):
+        if value.capitalize() in ["Small", "Medium", "Large"]:
+            self.size = value
+        else:
+            print("size must be Small, Medium, or Large")
+        
+    
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price = self.price + 1
+
+
